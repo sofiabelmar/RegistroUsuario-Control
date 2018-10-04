6b3e6d89c2fda7334f62959c3348f52ffab153c7
Pablo Sanchez <pablosanchez027@hotmail.com>
2018-10-03 23:55:18 -0700

    Add project files.

---
+++ RegistroUsuario-Control/ControlAlumno.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RegistroUsuario_Control
+{
+    /// <summary>
+    /// Interaction logic for ControlAlumno.xaml
+    /// </summary>
+    public partial class ControlAlumno : UserControl
+    {
+        public ControlAlumno()
+        {
+            InitializeComponent();
+        }
+
+        private void cb_CarreraAlumno_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
